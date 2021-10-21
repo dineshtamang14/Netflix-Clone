2b68c264a6341df888496e869d40d06e4b43de6f
Dinesh Tamang <dineshtamang7263@gmail.com>
2021-10-21 13:40:35 +0530

    add the row components

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -2,21 +2,32 @@ import React from 'react'
 import "./HomeScreen.css";
 import Nav from "./Nav"
 import Banner from "./Banner"
+import requests from './Request';
+import Row from './Row';
 
 function HomeScreen() {
     return (
-        <div className="HomeScreen">
-            {/* navbar */}
-            <Nav />
-        
-            {/* Banner */}
-            <Banner />
+      <div className="HomeScreen">
+        {/* navbar */}
+        <Nav />
 
-            {/* Row */}
-            {/* <Row /> */}
+        {/* Banner */}
+        <Banner />
 
-        </div>
-    )
+        {/* Row */}
+        <Row 
+            title="Trending Now" 
+            fetchUrl={requests.fetchTrending} 
+            isLargeRow
+        />
+        <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+        <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+        <Row title="Comdedy Movies" fetchUrl={requests.fetchComedyMovies} />
+        <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+        <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+        <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+      </div>
+    );
 }
 
 export default HomeScreen;
