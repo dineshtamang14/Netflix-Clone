ccff320735a15e6df880ec37dc9da979542cc1f7
Dinesh Tamang <dineshtamang7263@gmail.com>
2021-10-21 15:49:47 +0530

    add the home row

--- src/Row.js
+++ src/Row.js
@@ -21,7 +21,10 @@ function Row({title, fetchUrl, isLargeRow = false}) {
         <h2>{title}</h2>
 
         <div className="row__posters">
-          {movies.map((movie) => (
+          {movies.map(
+            (movie) =>
+              ((isLargeRow && movie.poster_path) ||
+                (!isLargeRow && movie.backdrop_path)) && (
             <img
               className={`row__poster ${isLargeRow && "row__posterLarge"}`}
               key={movie.id}
