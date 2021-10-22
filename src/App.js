0df24338fdc0adcd80021a4b0411f32f88da5a00
Dinesh Tamang <dineshtamang7263@gmail.com>
2021-10-22 12:28:22 +0530

    added the router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './App.css';
 import HomeScreen from "./HomeScreen"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-      <HomeScreen />
+      <Router>
+        <Switch>
+          <Route path="/">
+            <HomeScreen />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
