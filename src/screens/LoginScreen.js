a35297a1ba040626e763e29a13f902c872cf96dc
Dinesh Tamang <dineshtamang7263@gmail.com>
2021-10-24 21:40:13 +0530

    v2

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -3,8 +3,20 @@ import "./LoginScreen.css";
 
 function LoginScreen(){
     return (
-        <div>
-            div.
+        <div className="loginScreen">
+            <div className="loginScreen__background">
+                <img 
+                className="loginScreen__logo"
+                src="https://netflix-clone-asset.s3.ap-south-1.amazonaws.com/asset/logo.png" 
+                alt="Netflix-logo"
+                />
+                <button 
+                className="loginScreen__button">
+                Sign In
+                </button>
+            </div>
         </div>
     )
 }
+
+export default LoginScreen;
