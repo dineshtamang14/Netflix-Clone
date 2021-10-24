ea9220179cb91ffb98d2db625950cdd13a42bba8
Dinesh Tamang <dineshtamang7263@gmail.com>
2021-10-24 23:56:51 +0530

    added the sign in function

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,8 +1,9 @@
 import React, {useState} from "react";
 import "./LoginScreen.css";
+import SignInScreen from "./SignUpScreen";
 
 function LoginScreen(){
-    const [sign, setSignIn] = useState(false);
+    const [signIn, setSignIn] = useState(false);
     return (
         <div className="loginScreen">
             <div className="loginScreen__background">
@@ -18,29 +19,34 @@ function LoginScreen(){
                 </button>
                 
                 <div className="loginScreen__gradient" />
+            </div>
                 
                 <div className="loginScreen__body">
-                    <h1>Unlimited movies, TV shows and 
-                    more.</h1>
-                    <h2>Watch anywhere. Cancel anytime.</h2>
-                    <h3>
-                    Ready to watch? Enter your email to 
-                    create or restart your 
-                    membership.
-                    </h3>
+                    {signIn ? (
+                        <SignInScreen />
+                    ) : (
+                    <>
+                        <h1>Unlimited movies, TV shows and 
+                                 more.</h1>
+                        <h2>Watch anywhere. Cancel anytime.</h2>
+                        <h3>Ready to watch? Enter your email to 
+                            create or restart your 
+                            membership.
+                        </h3>
 
-                    <div className="loginScreen__input">
-                        <form>
-                            <input type="email" 
-                            placeholder="Email Address" />
-                            <button className="loginScreen__getStarted">
-                                GET STARTED
-                            </button>
-                        </form>
-                    </div>
-
-                </div>
-
+                        <div className="loginScreen__input">
+                            <form>
+                                <input type="email" 
+                                placeholder="Email Address" />
+                                <button 
+                                    onClick={() => setSignIn(true)}
+                                    className="loginScreen__getStarted">
+                                    GET STARTED
+                                </button>
+                            </form>
+                        </div>
+                    </>
+                )}
             </div>
         </div>
     )
---
+++ src/screens/SignUpScreen.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import "./SignUpScreen.css";
+
+function SignUpScreen() {
+    return (
+        <div className="signupScreen">
+            <form>
+                <h1>Sign In</h1>
+                <input placeholder="Email" type="email"/>
+                <input placeholder="Password" type="password"/>
+                <button type="submit">Sign In</button>
+
+                <h4>
+                    <span className="signupScreen__gray">New to Netflix?  </span>
+                 Sign Up now.</h4>
+            </form>
+        </div>
+    )
+}
+
+export default SignUpScreen;
