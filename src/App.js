bb092a38cd5b269480d77397170c10eae915eb99
Dinesh Tamang <dineshtamang7263@gmail.com>
2021-10-25 00:44:47 +0530

    added the redux

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
 import HomeScreen from "./screens/HomeScreen";
 import LoginScreen from "./screens/LoginScreen";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { auth } from './firebase';
 
 function App() {
   const user = null;
+
+  useEffect(()=>{
+    const unsubscribe = auth.onAuthStateChanged((userAuth)=>{
+      if(userAuth){
+        console.log(userAuth);
+      } else {
+        //loged out
+      }
+    })
+
+    return unsubscribe;
+  }, [])
+
   return (
     <div className="app">
       <Router>
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
     apiKey: "AIzaSyCV6imLl1QU6j1DRKgY7bZKR1oSYJWfoB8",
     authDomain: "netflix-clone-fe6cf.firebaseapp.com",
@@ -6,3 +8,12 @@ const firebaseConfig = {
     messagingSenderId: "1071096604529",
     appId: "1:1071096604529:web:de01b215673cc352f538d0"
   };
+
+
+const firebaseapp = firebase.initializeApp(firebaseConfig);
+const db = firebaseapp.firestore();
+const auth = firebase.auth();
+
+export {auth};
+export default db;
+
