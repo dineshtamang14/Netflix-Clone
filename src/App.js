918582b40f6cb44bcd611ab51f16064baf1e479b
Dinesh Tamang <dineshtamang7263@gmail.com>
2021-10-25 11:09:40 +0530

    add the sigin

--- src/App.js
+++ src/App.js
@@ -2,18 +2,27 @@ import React, {useEffect} from 'react';
 import './App.css';
 import HomeScreen from "./screens/HomeScreen";
 import LoginScreen from "./screens/LoginScreen";
+import { useDispatch, useSelector } from 'react-redux';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { auth } from './firebase';
+import { login, logout, selectUser } from "./features/userSlice";
 
 function App() {
-  const user = null;
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
 
   useEffect(()=>{
     const unsubscribe = auth.onAuthStateChanged((userAuth)=>{
       if(userAuth){
+        // login 
         console.log(userAuth);
+        dispatch(login({
+          uid: userAuth.uid,
+          email: userAuth.email,
+        }))
       } else {
         //loged out
+        dispatch(logout);
       }
     })
 
--- src/features/userSlice.js
+++ src/features/userSlice.js
@@ -37,20 +37,8 @@ export const userSlice = createSlice({
 });
 
 
-export const { increment, decrement, incrementByAmount } = counterSlice.actions;
+export const { login, logout } = userSlice.actions;
 
-// The function below is called a selector and allows us to select a value from
-// the state. Selectors can also be defined inline where they're used instead of
-// in the slice file. For example: `useSelector((state: RootState) => state.counter.value)`
-export const selectCount = (state) => state.counter.value;
+export const selectCount = (state) => state.user.user;
 
-// We can also write thunks by hand, which may contain both sync and async logic.
-// Here's an example of conditionally dispatching actions based on current state.
-export const incrementIfOdd = (amount) => (dispatch, getState) => {
-  const currentValue = selectCount(getState());
-  if (currentValue % 2 === 1) {
-    dispatch(incrementByAmount(amount));
-  }
-};
-
-export default counterSlice.reducer;
+export default userSlice.reducer;
