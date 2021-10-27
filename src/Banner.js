5c0e9adf39116c7e58fdea9cce693386dcc0cd90
Dinesh Tamang <dineshtamang7263@gmail.com>
2021-10-27 12:10:50 +0530

    added home page

--- src/Banner.js
+++ src/Banner.js
@@ -20,7 +20,7 @@ function Banner() {
   }, []);
 
   const truncate = (string, n) => {
-    return string.length > n ? string.substr(0, n - 1) + "..." : string;
+    return string?.length > n ? string.substr(0, n - 1) + "..." : string;
   };
 
   return (
--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import firebase from "firebase";
 // import "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCV6imLl1QU6j1DRKgY7bZKR1oSYJWfoB8",
-    authDomain: "netflix-clone-fe6cf.firebaseapp.com",
-    projectId: "netflix-clone-fe6cf",
-    storageBucket: "netflix-clone-fe6cf.appspot.com",
-    messagingSenderId: "1071096604529",
-    appId: "1:1071096604529:web:de01b215673cc352f538d0"
+  apiKey: "AIzaSyD9KVXEVFzImHaJI10nYm6747peKkuhzNA",
+  authDomain: "netflix-clone-c45d1.firebaseapp.com",
+  projectId: "netflix-clone-c45d1",
+  storageBucket: "netflix-clone-c45d1.appspot.com",
+  messagingSenderId: "694289365227",
+  appId: "1:694289365227:web:782dcb67534584947ce6ae",
 };
 
 
