deabfe758c38065cde738533fd8a4639fca1a156
Dinesh Tamang <dineshtamang7263@gmail.com>
2021-11-13 17:50:02 +0530

    added trailer features

--- src/Row.js
+++ src/Row.js
@@ -27,19 +27,19 @@ function Row({title, fetchUrl, isLargeRow = false}) {
     },
   };
 
-    const handleClick = (movie) =>{
-      if(trailerUrl){
-        setTrailerUrl('');
+	  const handleClick = (movie) => {
+      console.log(movie)
+      if (trailerUrl) {
+        setTrailerUrl("");
       } else {
-        movieTrailer(movie?.name || "")
-        .then((url) => {
-          const urlParams = new URLSearchParams(new URL(url).search);
-          setTrailerUrl(urlParams.get('v'));
-        }).catch(
-          err => console.log(err)
-        )
+        movieTrailer(movie?.title || "")
+          .then((url) => {
+            const urlParams = new URLSearchParams(new URL(url).search);
+            setTrailerUrl(urlParams.get('v'));
+          })
+          .catch((error) => console.log(error));
       }
-    }
+    };
 
     return (
       <div className="row">
